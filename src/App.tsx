30dc8367c837969b47fd71749cf0336362ab71fe
sveta <Sunkovasvetlana@gmail.com>
2021-12-06 12:23:11 +0300

    create css-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="app-wrapper">
+        <Header/>
+        <Navbar/>
+        <Profile/>
+      </div>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+
+        <nav className="nav">
+            <div className="item"><a>Profile</a></div>
+            <div className="item"><a>Messages</a></div>
+            <div className="item"><a>News</a></div>
+            <div className="item"><a>Music</a></div>
+            <div className="item"><a>Helping</a></div>
+        </nav>
+    )
+}
+
+export default Navbar;
